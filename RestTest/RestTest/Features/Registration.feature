09721bfb2ee0953cc5fcb441c44704b85f767c87
mcsymiv <45435157+mcsymiv@users.noreply.github.com>
2020-10-19 17:50:02 +0300

    Add Registration.Feature and RegistrationSteps

---
+++ RestTest/RestTest/Features/Registration.feature
@@ -0,0 +1,15 @@
+﻿Feature: Registration
+	In order to add my company
+	As a user
+	I want to register
+
+@register
+Scenario: Successful registration
+	Given Genereted user input <name> <email> <password>
+	Then I make doRegistration request with generated data
+	Then I get server response status code 200
+	Then I get server response in the body "name" : <name>
+	Then I get server response in the body "email" : <email>
+
+
+
---
+++ RestTest/RestTest/Steps/RegistrationSteps.cs
@@ -0,0 +1,52 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace RestTest.Steps
+{
+
+    [Binding]
+    public class RegistrationSteps
+    {
+        RestClient client;
+        RestRequest request;
+        Dictionary<string, string> UserData;
+
+        [Given(@"Genereted user input (.*) (.*) (.*)")]
+        public void GivenGeneretedUserInput(string p0, string p1, string p2)
+        {
+            client = new RestClient("http://users.bugred.ru");
+            string randomDateString = DateTime.Now.ToString()
+                .Replace(".", "")
+                .Replace("/", "")
+                .Replace("PM", "")
+                .Replace(":", "");
+            UserData = new Dictionary<string, string>
+            {
+                {"name", randomDateString },
+                {"email", randomDateString + "@" + randomDateString.Substring(0,4) + "." + randomDateString.Substring(0,3) },
+                {"password", randomDateString + "qwerty" }
+            };
+        }
+
+
+        [Then(@"I make doRegistration request with generated data")]
+        public void ThenIMakeDoRegistrationRequestWithGeneratedData()
+        {
+            request = new RestRequest(" /tasks/rest/doregister", Method.POST);
+        }
+        
+        [Then(@"I get server response status code (.*)")]
+        public void ThenIGetServerResponseStatusCode(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I get server response in the body ""(.*)"" : (.*)")]
+        public void ThenIGetServerResponseInTheBody(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
